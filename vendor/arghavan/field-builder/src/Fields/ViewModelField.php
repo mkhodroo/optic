0f9525d086b2140c86937adf1a28186c9a28b5b4
behinIndustrialGroup <166484385+behinIndustrialGroup@users.noreply.github.com>
2026-08-27 18:04:10 +0330

    Update ViewModelField.php

--- vendor/arghavan/field-builder/src/Fields/ViewModelField.php
+++ vendor/arghavan/field-builder/src/Fields/ViewModelField.php
@@ -3,7 +3,6 @@
 namespace MyFormBuilder\Fields;
 
 use Behin\SimpleWorkflow\Controllers\Core\ViewModelController;
-use Behin\SimpleWorkflow\Models\Core\Entity;
 use Behin\SimpleWorkflow\Models\Core\ViewModel;
 
 class ViewModelField extends AbstractField
@@ -16,67 +15,205 @@ class ViewModelField extends AbstractField
         $s = "";
 
         $viewModel = ViewModel::find($viewModelId);
-        $model = ViewModelController::getModelById($viewModelId);
+
         $columns = explode(',', $viewModel->default_fields);
-        $max_number_of_rows = $viewModel->max_number_of_rows;
 
-        $s .= "<div class='table-responsive card p-1' style='" . $style . "'>";
+        /*
+         * -----------------------------------------
+         * Header settings
+         * -----------------------------------------
+         */
 
-        $s .= "<div class='card-header d-flex align-items-center justify-content-between mb-2'>";
+        // رنگ هدر
+        // اگر ستون header_color وجود نداشته باشد یا خالی باشد
+        // رنگ پیش فرض استفاده می شود.
+        $headerColor = $viewModel->getAttribute('header_color');
 
-        // عنوان
-        $s .= "<h5 class='mb-0'>" . trans('fields.' . $viewModel->name) . "</h5>";
-
-        // دکمه‌ها
-        $s .= "<div class='d-flex align-items-center'>";
-
-        // دکمه ایجاد رکورد جدید
-        if ($viewModel->allow_create_row) {
-            $s .= "<div id='create-view-model-row-{$viewModel->id}'></div>";
+        if (empty($headerColor)) {
+            $headerColor = '#f5f5f5';
         }
 
-        // دکمه رفرش
+        // عنوان:
+        // اول label
+        // اگر label وجود نداشت -> name
+        $title = $viewModel->getAttribute('label');
+
+        if (empty($title)) {
+            $title = $viewModel->name;
+        }
+
+        $title = trans('fields.' . $title);
+
+
+        /*
+         * -----------------------------------------
+         * Main container
+         * -----------------------------------------
+         */
+
+        $s .= "<div class='table-responsive card p-0' style='" . $style . "'>";
+
+
+        /*
+         * -----------------------------------------
+         * Header
+         * -----------------------------------------
+         */
+
         $s .= "<div
-            class='ml-2'
-            style='cursor: pointer;'
-            onclick='get_view_model_rows(\"{$viewModel->id}\", \"{$viewModel->api_key}\")'
+            class='card-header d-flex align-items-center justify-content-between'
+            style='
+                background-color: {$headerColor};
+                min-height: 50px;
+                border-bottom: 1px solid rgba(0,0,0,.1);
+            '
+        >";
+
+
+        /*
+         * -----------------------------------------
+         * Title - Right
+         * -----------------------------------------
+         */
+
+        $s .= "<div class='d-flex align-items-center'>";
+
+        $s .= "<h5
+            class='mb-0 font-weight-bold'
+            style='color: #333;'
+        >";
+
+        $s .= e($title);
+
+        $s .= "</h5>";
+
+        $s .= "</div>";
+
+
+        /*
+         * -----------------------------------------
+         * Buttons - Left
+         * -----------------------------------------
+         */
+
+        $s .= "<div class='d-flex align-items-center'>";
+
+
+        /*
+         * Create button
+         *
+         * فعلاً یک container خالی است.
+         * getRows بر اساس can_create آن را پر می‌کند.
+         */
+
+        if ($viewModel->allow_create_row) {
+
+            $s .= "<div
+                id='create-view-model-row-{$viewModel->id}'
+                class='ml-2'
+            ></div>";
+        }
+
+
+        /*
+         * Refresh button
+         */
+
+        $s .= "<button
+            type='button'
+            class='btn btn-sm btn-secondary'
+            title='" . trans('fields.Refresh') . "'
+            onclick='get_view_model_rows(
+                \"{$viewModel->id}\",
+                \"{$viewModel->api_key}\"
+            )'
         >";
 
         $s .= "<i class='fa fa-refresh'></i>";
 
-        $s .= "</div>";
+        $s .= "</button>";
+
 
         $s .= "</div>";
 
+        /*
+         * End Header
+         */
+
         $s .= "</div>";
 
+
+        /*
+         * -----------------------------------------
+         * Content
+         * -----------------------------------------
+         */
 
         if ($viewModel->show_as == 'table') {
-            $s .= "<table class='table' id='{$viewModel->id}' style='width: 100%'>";
-            $s .= "<thead><tr>";
-            foreach ($columns as $column) {
-                $columnLabel = trans("fields." . $column);
-                $s .= "<th style='border-top: 0px;border-left: solid gray 1px;'>$columnLabel</th>";
-            }
-            $s .= "<th style='border-top: 0px;'></th>";
-            $s .= "</tr></thead>";
-            $s .= "<tbody></tbody>";
-            $s .= "</table>";
-        } elseif ($viewModel->show_as == 'box') {
-            $s .= "<div class='' id='{$viewModel->id}' style='width: 100%'>";
 
-            $s .= "</div>";
+            $s .= "<table
+                class='table mb-0'
+                id='{$viewModel->id}'
+                style='width: 100%'
+            >";
+
+            $s .= "<thead><tr>";
+
+            foreach ($columns as $column) {
+
+                $column = trim($column);
+
+                $columnLabel = trans("fields." . $column);
+
+                $s .= "<th
+                    style='
+                        border-top: 0;
+                        border-left: solid gray 1px;
+                    '
+                >";
+
+                $s .= e($columnLabel);
+
+                $s .= "</th>";
+            }
+
+            $s .= "<th style='border-top: 0;'></th>";
+
+            $s .= "</tr></thead>";
+
+            $s .= "<tbody></tbody>";
+
+            $s .= "</table>";
+
+        } elseif ($viewModel->show_as == 'box') {
+
+            $s .= "<div
+                id='{$viewModel->id}'
+                style='width: 100%'
+            ></div>";
         }
 
 
-
-
-
-
+        /*
+         * -----------------------------------------
+         * End container
+         * -----------------------------------------
+         */
 
         $s .= "</div>";
 
-        $s .= "<script>get_view_model_rows(`$viewModel->id`, `$viewModel->api_key`)</script>";
+
+        /*
+         * Load rows
+         */
+
+        $s .= "<script>
+            get_view_model_rows(
+                `{$viewModel->id}`,
+                `{$viewModel->api_key}`
+            )
+        </script>";
+
 
         return $s;
     }
