34100050211e80fb30cd896806d405b5131f6c3c
behinIndustrialGroup <166484385+behinIndustrialGroup@users.noreply.github.com>
2026-08-27 16:31:49 +0330

    Update ViewModelField.php

--- vendor/arghavan/field-builder/src/Fields/ViewModelField.php
+++ vendor/arghavan/field-builder/src/Fields/ViewModelField.php
@@ -23,7 +23,7 @@ class ViewModelField extends AbstractField
         $s .= "<div class='table-responsive card p-1' style='" . $style . "'>";
 
         // ✅ اضافه کردن دکمه رفرش بالا
-        $s .= "<div class='d-flex align-items-center mb-2'>";
+        $s .= "<div class='card-header d-flex align-items-center mb-2'>";
         $s .= "<button type='button' class='btn btn-sm ' onclick='get_view_model_rows(\"$viewModel->id\", \"$viewModel->api_key\")'>";
         $s .= "<i class='fa fa-refresh'></i> ";
         $s .= "</button>";
